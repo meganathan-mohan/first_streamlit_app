952ce6b123462c48be28d4465c96d0a5f87f1639
Meganathan <meganathan-mohan@users.noreply.github.com>
2023-11-14 11:56:14 +0530

    Update streamlit_app.py
    updated my breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-
+streamlit.header('Breakfast Menu')
+steramlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smolothee')
+streamlit.text('Hard boiled free range eggs')
 
