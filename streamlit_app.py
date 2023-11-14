abdb9c8302ea13253864243481be9dfe8831c039
Meganathan <meganathan-mohan@users.noreply.github.com>
2023-11-14 12:00:01 +0530

    Update streamlit_app.py
    updated toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach, & Rocket Smolothee')
-streamlit.text('Hard boiled free range eggs')
-
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach, & Rocket Smolothee')
+streamlit.text('🐔 Hard boiled free range eggs')
+streamlit.text('🥑🍞 Avocado Toast')
